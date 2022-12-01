7d797dfdd73ec748d197f5b267bc923c7a7bb126
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-12-01 19:12:19 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , {useState} from "react";
 import "./style.css";
 import Navbar from './Navbar'
+import Shop from './Shop'
 
 export default function App() 
 {
@@ -11,7 +12,7 @@ export default function App()
   return (
     <div>
       <Navbar/>
-      <p>Start editing to see some magic happen :)</p>
+      <Shop/>
     </div>
   );
 }
--- src/Shop.js
+++ src/Shop.js
@@ -1,1 +1,12 @@
-imp
+import React from 'react';
+
+export default function Shop()
+{
+  return(
+     <div class = 'container'> 
+     <button type="button" class="btn btn-primary">+</button>
+     Add this item to cart
+     <button type="button" class="btn btn-primary">-</button>
+     </div>
+  )
+}
